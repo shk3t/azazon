ad3ee1a90cfffe3b451e890e0c5fa4d1d73fc81f
shk3t <daniildudov@gmail.com>
2025-05-14 19:15:05 +0300

    clean

--- auth/internal/router/router.go
+++ auth/internal/router/router.go
@@ -8,26 +8,4 @@ import (
 func SetupRoutes() {
 	http.HandleFunc("POST /register", handler.Register)
 	http.HandleFunc("POST /login", handler.Login)
-
-	// api.Use(jwtware.New(jwtware.Config{
-	// 	SigningKey:  jwtware.SigningKey{Key: []byte(config.Env.SecretKey)},
-	// 	TokenLookup: "header:Authorization,cookie:Authorization",
-	// }))
-
-	// api.Get("/projects", handler.GetProjects)
-	// api.Get("/projects/:id", handler.GetProject)
-	//
-	// api.Get("/tasks", handler.GetTasks)
-	// api.Get("/tasks/:id", handler.GetTask)
-	//
-	// api.Get("/solutions", handler.GetSolutions)
-	// api.Get("/solutions/:id", handler.GetSolution)
-	//
-	// api.Post("/projects", handler.LoadProject)
-	// api.Put("/projects/:id", handler.LoadProject)
-	// api.Delete("/projects/:id", handler.DeleteProject)
-	// api.Post("/solutions", handler.SubmitSolution)
-	//
-	// api.Get("/delayed-tasks", handler.GetDelayedTasks)
-	// api.Get("/delayed-tasks/:id", handler.GetDelayedTask)
 }
